b480f15e3203062fe4d67fa9bbf1762048bf7a62
Prashant Thapa <tprasant10@gmail.com>
2024-09-22 00:37:10 +0100

    add session storage in dashboard page

---
+++ frontend/src/components/SchedulesDetail.jsx
@@ -0,0 +1,26 @@
+const SchedulesDetail = ({ schedule }) => {
+  const {
+    name,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+  } = schedule;
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{Monday}</td>
+      <td>{Tuesday}</td>
+      <td>{Wednesday}</td>
+      <td>{Thursday}</td>
+      <td>{Friday}</td>
+      <td>{Saturday}</td>
+      <td>{Sunday}</td>
+    </tr>
+  );
+};
+
+export default SchedulesDetail;
